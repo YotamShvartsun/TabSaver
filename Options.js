839c9e4aac93255c1a4d7caf4042d3e1136b8902
YotamShvartsun <34839949+YotamShvartsun@users.noreply.github.com>
2018-05-02 14:20:08 +0300

    fixed options bug

--- Options.js
+++ Options.js
@@ -21,8 +21,7 @@ $(function () {
         chrome.tabs.create({ url: "https://github.com/YotamShvartsun/TabSaver" });
     });
     $("#Reset_All").click(function () {
-        localStorage["TabSaver!!_Action"] = JSON.stringify("Reset Tab Saver?");
-        location.href = "delete.html";
+        deletepage();
     });
     $("#Rateus").click(function () {
         chrome.tabs.create({ url: "https://chrome.google.com/webstore/detail/tab-saver/ipmohfoikjagbhanbnknpbkkdaimfbcl/reviews" });
